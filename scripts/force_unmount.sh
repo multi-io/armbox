2c7fc80ae4745ad80ba2ea40355493c9bf052839
Olaf Klischat <olaf.klischat@gmail.com>
2017-09-13 11:08:50 +0000

    try to unmout residual sdcard filesystems before startup

--- scripts/force_unmount.sh
+++ scripts/force_unmount.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
 
+for branch in default next; do
+  sudo umount -l /home/ubuntu/.tmp/rootfs-${branch}-lamobo-r1-jessie-no/tmp/overlay
+  sudo umount -l /home/ubuntu/.tmp/rootfs-${branch}-lamobo-r1-jessie-no
+done
