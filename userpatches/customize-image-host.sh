b0288be25d8d16d62903eba3fac10d1ae8a80833
Olaf Klischat <olaf.klischat@gmail.com>
2017-09-13 14:21:02 +0000

    customize-image-host: use my integration branch of Ansible

--- userpatches/customize-image-host.sh
+++ userpatches/customize-image-host.sh
@@ -7,7 +7,7 @@ set -e
 apt-get install --yes python-pip
 ans_dir=/root/ansible
 if [ ! -d "$ans_dir" ]; then
-    git clone https://github.com/multi-io/ansible.git "$ans_dir"
+    git clone --branch myintegration https://github.com/multi-io/ansible.git "$ans_dir"
     pip install -r "$ans_dir/requirements.txt"
 fi
 source "$ans_dir/hacking/env-setup"
