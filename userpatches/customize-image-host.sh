72cdb94332df6c45b0641d1ad34615aa3997689b
Olaf Klischat <olaf.klischat@gmail.com>
2017-09-13 11:10:07 +0000

    customize-image-host: install recent enough ansible (my fork for now)

--- userpatches/customize-image-host.sh
+++ userpatches/customize-image-host.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
-#!/bin/sh
-
 set -e
+
+# apt-get install --yes ansible  # too old
+
+apt-get install --yes python-pip
+ans_dir=/root/ansible
+if [ ! -d "$ans_dir" ]; then
+    git clone https://github.com/multi-io/ansible.git "$ans_dir"
+    pip install -r "$ans_dir/requirements.txt"
+fi
+source "$ans_dir/hacking/env-setup"
 
 # the , after the chroot location is vital...otherwise ansible bails out somehow
 ansible-playbook -c chroot  -i "$CACHEDIR/$SDCARD", "${SRC}/userpatches/routerbox/setup.yml"
