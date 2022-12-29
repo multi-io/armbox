e71d08710941332d2541655ebb0ce4024ed30561
Olaf Klischat <olaf.klischat@gmail.com>
2022-12-29 01:07:44 +0000

    customize-image-host.sh: install newer fork of ansible
    fork is still needed to make systemd module work in a chroot

--- userpatches/customize-image-host.sh
+++ userpatches/customize-image-host.sh
@@ -1,16 +1,23 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
 # apt-get install --yes ansible  # too old
 
-apt-get install --yes python-pip
+apt update
+apt install --yes python3-pip python-is-python3
+
 ans_dir=/root/ansible
 if [ ! -d "$ans_dir" ]; then
-    git clone --branch myintegration https://github.com/multi-io/ansible.git "$ans_dir"
+    git clone --depth 1 --branch armbox https://github.com/multi-io/ansible.git "$ans_dir"
     pip install -r "$ans_dir/requirements.txt"
 fi
+set +e
 source "$ans_dir/hacking/env-setup"
+set -e
+
+# needed for the -c chroot (chroot connection plugin)
+ansible-galaxy collection install community.general
 
 # the , after the chroot location is vital...otherwise ansible bails out somehow
 ansible-playbook ${ANSIBLE_PARAMS} -c chroot -e board=${BOARD}  -i "$SDCARD", "${SRC}/userpatches/routerbox/setup.yml"
