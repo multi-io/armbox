d66bdf144bff0174658a8f22ddb70e360fbbe49a
Olaf Klischat <olaf.klischat@gmail.com>
2023-01-01 01:21:10 +0000

    customize-image-host.sh: recreated python->python3 symlink in case prepare_host overwrote it

--- userpatches/customize-image-host.sh
+++ userpatches/customize-image-host.sh
@@ -7,6 +7,10 @@ set -e
 apt update
 apt install --yes python3-pip python-is-python3
 
+# lib/functions/host/prepare-host.sh may have rewritten the symlink to point to python2.7, the apt install above wouldn't have fixed that
+# if this turns out to be problematic, we might use a python3 venv instead
+ln -fs /usr/bin/python3 /usr/bin/python
+
 ans_dir=/root/ansible
 if [ ! -d "$ans_dir" ]; then
     git clone --depth 1 --branch armbox https://github.com/multi-io/ansible.git "$ans_dir"
